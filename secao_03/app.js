752bcce613732e3222a648fd658e5fc907071246
Fábio Henrique <fabiosjc@gmail.com>
2017-06-29 23:33:07 -0300

    Restarting app with nodemon

--- secao_03/app.js
+++ secao_03/app.js
@@ -2,12 +2,15 @@ console.log('Starting app')
 const fs = require('fs')
 const os = require('os')
 const notes = require('./notes.js')
+const _ = require('lodash')
 
 var user = os.userInfo()
 var res = notes.addNote()
 console.log(res);
 var res = notes.add(9, 7)
 console.log(res);
+
+console.log(_.uniq([1,3,4,1,4,3]))
 
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function (err) {
 //   if (err) {
